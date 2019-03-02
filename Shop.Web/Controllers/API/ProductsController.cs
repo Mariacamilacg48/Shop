d5a2539dc9b247a59db46ee43185a33f436ff15d
Mariacamilacg48 <47586102+Mariacamilacg48@users.noreply.github.com>
2019-03-02 15:40:17 -0500

    API Products controller

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿namespace Shop.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Data;
+
+    //Rutear el API, cuando publiquemos el sitio web quedará así
+
+    [Route("api/[Controllers]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+
+        //Método naturaleza get, get de mi controlador
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return this.Ok(this.productRepository.GetAll());
+        }
+
+
+    }
+}
